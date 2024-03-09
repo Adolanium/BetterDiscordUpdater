4fa9ce375335fd3c5f32a0dd3c145e15a866efb2
Adolanium <94890352+Adolanium@users.noreply.github.com>
2024-03-10 00:02:48 +0200

    Resolved Discord startup bug with configured version

--- BetterDiscordUpdater/DiscordManager.cs
+++ BetterDiscordUpdater/DiscordManager.cs
@@ -26,22 +26,33 @@ namespace BetterDiscordUpdater
             var config = Configuration.LoadFromFile(ConfigFilePath);
             var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
             var discordPath = Path.Combine(localAppData, config.DiscordVersion);
-
             var appDirs = Directory.GetDirectories(discordPath)
                 .Select(Path.GetFileName)
                 .Where(name => name.StartsWith("app"))
                 .OrderBy(name => name)
                 .ToList();
 
-            var lastAppDir = appDirs.Last();
-            var discordExePath = Path.Combine(discordPath, lastAppDir, "DiscordPTB.exe");
+            if (appDirs.Count > 0)
+            {
+                var lastAppDir = appDirs.Last();
+                var discordExePath = Path.Combine(discordPath, lastAppDir, $"{config.DiscordVersion}.exe");
 
-            var processStartInfo = new ProcessStartInfo(discordExePath);
-            processStartInfo.UseShellExecute = true;
-            Process.Start(processStartInfo);
-
-            Environment.Exit(0);
+                if (File.Exists(discordExePath))
+                {
+                    var processStartInfo = new ProcessStartInfo(discordExePath);
+                    processStartInfo.UseShellExecute = true;
+                    Process.Start(processStartInfo);
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine($"Discord executable not found at: {discordExePath}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No app directory found in: {discordPath}");
+            }
         }
-
     }
 }
